86a544b94352b5d82f6ef09d8927cb8e7e6ce47c
Noah Friedman <noahfriedman2@gmail.com>
2022-12-11 15:01:35 -0500

    Rename config.go, Config type to server.go, Server, add interface for Server.Start method

--- server.go
+++ server.go
@@ -1,7 +1,13 @@
 package speelcraft_mgr
 
-// Config represents an imported JSON file with configuration information for the program.
-type Config struct {
+import "net/http"
+
+// Server represents an instance of a web server that allows clients to download the mods directly from the Minecraft
+// server's 'mods' folder.
+type Server struct {
+	// server is the internal http.Server instance.
+	server http.Server
+
 	// Path is the absolute system path to the folder containing the server executable (.jar) and 'mods' folder.
 	Path string `json:"path"`
 
@@ -13,4 +19,12 @@ type Config struct {
 
 	// CertFile is the absolute system path to the certificate to be used for TLS encryption (HTTPS).
 	CertFile string `json:"certfile"`
+}
+
+// Start starts the web server.
+//
+// The function will return an error if Server.UseHTTPS is `true` but Server.KeyFile and/or Server.CertFile are not
+// properly configured.
+func (s *Server) Start() error {
+	return nil
 }
