1cf8cc8b1034b10b4d6892e004ade7fe4bb9f2ea
Noah Friedman <noahfriedman2@gmail.com>
2022-12-11 15:09:12 -0500

    Update documentation in server.go [skip ci]

--- server.go
+++ server.go
@@ -8,7 +8,7 @@ type Server struct {
 	// server is the internal http.Server instance.
 	server http.Server
 
-	// Path is the absolute system path to the folder containing the server executable (.jar) and 'mods' folder.
+	// Path is the absolute system path to the folder containing the server executable (`.jar`) and `mods` folder.
 	Path string `json:"path"`
 
 	// UseHTTPS is a boolean value that determines whether the web server will use TLS encryption.
@@ -22,9 +22,10 @@ type Server struct {
 }
 
 // Start starts the web server.
-//
-// The function will return an error if Server.UseHTTPS is `true` but Server.KeyFile and/or Server.CertFile are not
-// properly configured.
+// The function will return an error if:
+//   - Server.Path is not a valid path, is not a folder, or does not contain a `mods` subdirectory.
+//   - Server.UseHTTPS is `true` but Server.KeyFile and/or Server.CertFile are not
+//     properly configured.
 func (s *Server) Start() error {
 	return nil
 }
